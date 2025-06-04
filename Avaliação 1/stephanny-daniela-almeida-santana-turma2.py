cbbb3bbb6bf1601f5a82c049ada6cbfeede5e372
stephanny31 <stephannydanielatete@gmail.com>
2025-06-04 11:07:54 -0300

    Stephanny Daniela Almeida Santana - Turma 2

---
+++ Avaliação 1/stephanny-daniela-almeida-santana-turma2.py
@@ -0,0 +1,17 @@
+altura = float(input ("Digite a sua altura: "))
+peso = float(input ("Digite o seu peso: "))
+
+imc = peso / (altura * altura)
+
+if imc < 18.5:
+    print("Abaixo do peso.")
+elif imc < 24.9:
+    print("Peso normal.")
+elif imc < 29.9:
+    print("Sobrepeso.")
+elif imc < 34.9:
+    print("Obesidade Grau I.")
+elif imc < 39.9:
+    print("Obesidade Grau II.")
+else:
+    print("Obesidade Grau III.")
